c06b7fcaa8a52bfb717e9fe8339be113b9f7979d
yoji-kojio <yoji.kojio@gmail.com>
2021-11-30 22:08:26 -0300

    :sparkles: Create Vuex mutations, getters and actions
    Uses vuex to handle data fetching and control state from todo list items.

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,40 @@
 import { createStore } from 'vuex'
+import axios from "axios";
 
 export default createStore({
   state: {
+    todoList: [],
+  },
+  getters: {
+    orderedTodoList(state) {
+      return state.todoList.sort((a,b) => (a.done > b.done) ? 1 : ((b.done > a.done) ? -1 : 0))
+    },
+    completedTodoList(state) {
+      return state.todoList.filter(item => item.done === true);
+    }
   },
   mutations: {
+    setTodoList(state, list) {
+      state.todoList = list;
+    },
+    insertTodoItem(state, item) {
+      state.todoList = [...state.todoList, item];
+    },
+    updateTodoItemDone(state, { itemId, isDone }) {
+      const index = state.todoList.findIndex(item => item.id === itemId);
+      state.todoList[index].done = isDone;
+    },
+    removeItemFromList(state, { itemId }) {
+      state.todoList = state.todoList.filter(item => item.id !== itemId);
+    }
   },
   actions: {
+    async fetchTodoList({ commit }) {
+      await axios.get(`http://localhost:3000/todos`).then(response => {
+        commit('setTodoList', response.data);
+      }).catch(error => {
+        console.error(error);
+      });
+    }
   },
-  modules: {
-  }
 })
