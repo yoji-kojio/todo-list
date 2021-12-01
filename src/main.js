fb80408adab3d6ecef25d4007a7d7d482c3679ed
yoji-kojio <yoji.kojio@gmail.com>
2021-11-30 22:09:18 -0300

    :wrench: Updates main.js to add v-tooltip, fontawesome and load icons

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faPlus,
+  faArrowCircleRight,
+  faCheckCircle,
+  faTrash
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import VTooltip from 'v-tooltip'
 
-createApp(App).use(store).mount('#app')
+import 'v-tooltip/dist/v-tooltip.css'
+
+library.add(faPlus, faArrowCircleRight, faCheckCircle, faTrash)
+
+const app = createApp(App);
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.use(VTooltip)
+app.use(store)
+
+app.mount('#app')
